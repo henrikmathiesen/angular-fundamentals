1ad4bf9c76140fb111a0bdea5be33c46ed658857
Henrik <henrik_mathiesen@hotmail.com>
2015-08-23 14:29:42 +0200

    Added jQuery CDN and intellisense file

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 /// <reference path="../typings/angularjs/angular.d.ts"/>
+/// <reference path="../typings/jquery/jquery.d.ts"/>
+
+
 (function(){
 	
 	angular.module('eventsApp', ['ngRoute', 'ngSanitize', 'ui.bootstrap']);
+	
+	
+	
 	
 })();
