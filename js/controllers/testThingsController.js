7465392077c5dd95846583f8dc8fc91f67557e3d
Henrik Mathiesen <henrik_mathiesen@hotmail.com>
2015-08-31 14:33:25 +0200

    Testing things; filter, cacheFactory, compile

--- js/controllers/testThingsController.js
+++ js/controllers/testThingsController.js
@@ -1,10 +1,93 @@
 (function(){
 	
-	var testThingsController = function($scope, valueFactory){
-		valueFactory.setTitle('Test Things');
+	//jQuery (# Testing Array Filter)
+	// ==============================================================================
+	var names = [
+		'Karl',
+		'Henrik',
+		'Mathiesen'
+	];
+	
+	var loopOutMarkUp = function(filteredName){
+		var markup = '';
+		
+		if(!filteredName) {
+			for (var index = 0; index < names.length; index++) {
+				markup += '<li>' + names[index] + '</li>';
+			}
+		}
+		else {
+			markup = '<li>' + filteredName + '</li>';
+		}
+		
+		$('ul[data-names]').html(markup);
 	};
 	
+	loopOutMarkUp();
+	
+	$('input[data-names-filter]').on('keyup', function(){
+		if(!$(this).val()) {
+			loopOutMarkUp();
+			return;
+		}
+		
+		var indexToFilter = names.indexOf($(this).val());
+		if(indexToFilter >= 0) {
+			//var filteredName = names.splice(indexToFilter, 1);
+			var filteredName = names[indexToFilter];
+			loopOutMarkUp(filteredName);
+		}
+	});
+	// ==============================================================================
+	
+	
+	var testThingsController = function($scope, $cacheFactory, $compile, valueFactory){
+		valueFactory.setTitle('Test Things');
+		
+		
+		// Testing Angulars $cacheFactory
+		
+		var myCache = $cacheFactory('myCache', { capacity:3 });
+		
+		$scope.addToCache = function(key, value){
+			myCache.put(key, value);
+		};
+		
+		$scope.readFromCache = function(key){
+			return myCache.get(key);
+		};
+		
+		$scope.getCacheStats = function(){
+			return myCache.info();
+		};
+		
+		
+		// # Testing Array Filter
+		
+		$scope.names = [
+			'Karl',
+			'Henrik',
+			'Mathiesen'
+		];
+		
+		
+		// Testing Angulars $compile - when we want to insert markup with bindings into DOM
+		
+		$scope.cname = "Bumle";
+		$scope.cHello = function(){ console.log("Hello"); };
+		
+		// Here we do not compile the markup, so bindings wont work
+		var cmarkup = '<span ng-click="cHello()"> {{ cname }} </span>';
+		angular.element('div[data-not-compiled]').html(cmarkup);
+		
+		// If we compile the markup, the bindings will work
+		cmarkup = $compile(cmarkup)($scope);
+		angular.element('div[data-compiled]').html(cmarkup);
+	};
+	
+	
+	
 	angular.module('eventsApp').controller('testThingsController', testThingsController);
-	testThingsController.$inject = ['$scope', 'valueFactory']
+	testThingsController.$inject = ['$scope', '$cacheFactory', '$compile', 'valueFactory'];
 	
 })();
