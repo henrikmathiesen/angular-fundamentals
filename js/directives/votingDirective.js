85219c5a7ed9e68e3aa76da1541c541cc4640678
Henrik <henrik_mathiesen@hotmail.com>
2015-09-20 12:10:50 +0200

    -comments

--- js/directives/votingDirective.js
+++ js/directives/votingDirective.js
@@ -1,11 +1,12 @@
 (function(){
 	
 	// This is a losly coupled directive, it focuses on voting up/down and display votes, with a template
-	// The directives lets the consuming send in parameters, from markup, that becomes its scope
+	// The directives lets the consumer send in parameters, from markup, that is used in the consumers scope
 	// We have one use for this directive, so far, and that is to vote on event sessions
 	// The directive, thanks to its lose coupeling, can be used to vote on other things as well
 	//
-	// Its scope is completely isolated from the parent controllers scope
+	// Its scope is completely isolated from the parent controllers scope, except for the passed parameters
+	// So 'scope' in a linking function will belong to this directive only
 	
 	
 	var votingDirective = function(){
@@ -20,7 +21,6 @@
 	};
 	
 	angular.module('eventsApp').directive('afVoting', votingDirective);
-	//votingDirective.$inject = []
 	
 })();
 
